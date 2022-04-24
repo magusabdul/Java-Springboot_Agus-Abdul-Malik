a337dde27219e25de1ac78f75a340208c3d6a7a1
magusabdul <magusabdul@gmail.com>
2022-04-25 01:57:14 +0700

    add folder praktikum latihan section 23

---
+++ 23_Configure Swagger Documentation Spring Boot/praktikum/swagger-doc/src/main/java/com/magusabdul/swaggerdoc/SwaggerDocApplication.java
@@ -0,0 +1,13 @@
+package com.magusabdul.swaggerdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerDocApplication.class, args);
+	}
+
+}
---
+++ 23_Configure Swagger Documentation Spring Boot/praktikum/swagger-doc/src/test/java/com/magusabdul/swaggerdoc/SwaggerDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magusabdul.swaggerdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
